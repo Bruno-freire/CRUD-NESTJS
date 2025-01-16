6fc6092bf67f7aef57902a5ed71114b22a2acff8
bruno <brunofreire1717@gmail.com>
2025-01-15 22:15:13 -0500

    feat: enableShutdownHooks updated

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe())
-  app.enableShutdownHooks()
+  
   // app.useGlobalInterceptors(new LogInterceptor())
   await app.listen(3000);
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
+import { INestApplication, Injectable, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
@@ -8,4 +8,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit{
     await this.$connect();  
   }
 
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      await app.close()
+    })
+  }
 }
